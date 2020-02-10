5f9d835c0511b10ae28d1eb8e4d57f88abd75481
王　詩博 <sibo_wang@ot.olympus.co.jp>
2020-02-10 15:23:06 +0900

    init

---
+++ rtcclient/client.py
@@ -0,0 +1,17 @@
+# coding:utf-8
+
+import requests
+
+class RTCClient:
+
+    def __init__(self, repository, username, password):
+
+        self.repository = repository
+        self.username = username
+        self.password = password
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
