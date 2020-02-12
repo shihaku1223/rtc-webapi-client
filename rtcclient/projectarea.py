5a17459ce3ca7f03559dcd422317a1fa0cab1937
王　詩博 <sibo_wang@ot.olympus.co.jp>
2020-02-12 13:27:18 +0900

    update projectarea.py

--- rtcclient/projectarea.py
+++ rtcclient/projectarea.py
@@ -14,3 +14,21 @@ class ProjectArea:
 
         self.title = jsonDict['dc:title']
         self.description = jsonDict['dc:description']
+
+    def retrieveWorkItems(self):
+        pass
+
+    def workItemsServices(self):
+        url = self.repository + '/oslc/contexts/{}/workitems/services.xml'
+        _headers = {}
+        _headers['Accept'] = 'application/xml'
+        _headers['OSLC-Core-version'] = '2.0'
+
+        request = RequestBuilder('GET',
+            url,
+            headers = _headers
+            ).build()
+        response = self.sendRequest(request)
+
+        dict = xmltodict.parse(response.text)
+        return dict
